5ab2d76aa0a7225e7975ad44142823c8806febab
JuKyYoon <yoongh97@gmail.com>
2021-05-03 22:51:58 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -37,23 +37,21 @@ app.use(express.static(path.join(__dirname, '../build')))
 // set the secret key variable for jwt
 app.set('jwt-secret', config.secret);
 
-// app.use(
-// 	expressSession({
-// 	    resave: false,
-// 	    saveUninitialized: false,
-// 	    secret: process.env.COOKIE_SECRET,
-// 	    proxy: true,
-// 	    cookie: {
-// 		    httpOnly: true,
-// 		    secure: process.env.NODE_ENV === 'development' ? false : true,
-// 		    sameSite: process.env.NODE_ENV === 'development' ? false : 'none'
-// 		},
-// 	})
-// );
-
 // router
 app.use('/', require('./routes/index'));
-
+app.use(
+	expressSession({
+	    resave: false,
+	    saveUninitialized: false,
+	    secret: process.env.COOKIE_SECRET,
+	    proxy: true,
+	    cookie: {
+		    httpOnly: true,
+		    secure: process.env.NODE_ENV === 'development' ? false : true,
+		    sameSite: process.env.NODE_ENV === 'development' ? false : 'none'
+		},
+	})
+);
 // open server
 app.listen(port, () => {
     console.log(`express is running on ${port}`);
