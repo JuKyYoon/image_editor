8f18fffaa0861686a349c6df365ef29561714642
JuKyYoon <yoongh97@gmail.com>
2021-06-01 15:29:52 +0900

    2021.06.01 15:29

--- server/app.js
+++ server/app.js
@@ -51,15 +51,15 @@ app.set('jwt-secret', config.secret);
 app.use('/', require('./routes/index'));
 
 
-// var host = process.env.HOST || '127.0.0.1';
-// var cors_proxy = require('cors-anywhere');
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2']
-// }).listen(port, host, function() {
-//     console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
+var host = process.env.HOST || '127.0.0.1';
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(9544, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
 
 // open server
 app.listen(port, () => {
